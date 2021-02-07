114d7f302e3c38f06ac1dea9d9a47f510fae1218
TOLGA <tolgaand@yandex.com>
2021-02-07 20:09:55 +0300

    Create a user welcome command scheme for testing

---
+++ bot/commands/welcomeMessage.js
@@ -0,0 +1,24 @@
+const MessageEmbed = require("discord.js").MessageEmbed;
+
+module.exports = {
+    name: "Hoşgeldin Mesajı",
+    aliases: ["hg"],
+    description: "Hoşgeldin Mesajı",
+    icon: "",
+    usage: ``,
+    active: true,
+    website: {
+        active: false
+    },
+    maintenance: {
+        active: false,
+        name: "Bakım modu",
+        description: "bakım",
+    },
+    type: 0,
+    createdUser: {
+        nickname: "tolgaand"
+    },
+    categoryID: "601ed99850cd0f362c2f3dc7",
+    async execute() {},
+};

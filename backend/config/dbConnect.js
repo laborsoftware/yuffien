eef62bb0854db77002523e522504191db297fd5e
TOLGA <tolgaand@yandex.com>
2021-02-07 20:01:59 +0300

    mongoDB connection

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+(function() {
+    mongoose.connect(
+        process.env.MONGODB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
+    );
+
+    mongoose.connection.on("open", () => {
+        console.log("MongoDB: Connected");
+    });
+    mongoose.connection.on("error", (err) => {
+        console.log("MongoDB: Error", err);
+    });
+
+    mongoose.Promise = global.Promise;
+
+}())

71c675eff8edbafab713a9bf0720243977b456a0
TOLGA <tolgaand@yandex.com>
2021-02-07 20:19:21 +0300

    Implement socket.io connection

---
+++ backend/socket/connection.js
@@ -0,0 +1,22 @@
+const io = require('socket.io')
+let socketServer = null
+
+module.exports = (app, server) => {
+    if (socketServer) return socketServer
+
+    socketServer = io(server)
+    socketServer.on('connection', socket => {
+        console.log("a user connect")
+
+        socket.on("join", id => {
+            socket.join(id);
+        })
+
+        socket.on("information", (data) => {
+            const id = data._id || data.id;
+            socketServer.to(id).emit("information", data)
+        })
+
+    })
+    return socketServer
+}

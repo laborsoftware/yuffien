8af795f5e613b771a46bce0bd0d6c5f087b7aaf7
TOLGA <tolgaand@yandex.com>
2021-02-07 20:21:25 +0300

    Add user, command and command category models

---
+++ backend/models/Command.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/*
+    type: 0 --> log(kullanıcı bir komut yazmadan calısan komut),
+    type: 1 --> command(kullanıcı bir komut yazınca calısan komut),
+    ...
+*/
+
+const commandSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    shortName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fileName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String
+    },
+    website: {
+        active: {
+            type: Boolean,
+            required: true
+        },
+    },
+    maintenance: {
+        active: {
+            type: Boolean,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+    },
+    updateLog: {
+        type: Array
+    },
+    type: {
+        type: Number,
+        enum: [0, 1],
+        required: true
+    },
+    aliases: {
+        type: Array,
+        unique: true
+    },
+    createdUser: {
+        nickname: {
+            type: String,
+            required: true
+        },
+    },
+    data: {
+        type: Object
+    },
+    categoryID: {
+        type: Schema.ObjectId,
+        required: true
+    },
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("command", commandSchema)
---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    locale: {
+        type: String,
+        required: true
+    },
+    guilds: {
+        type: Array,
+        required: true
+    },
+    onlyOwnerGuilds: {
+        type: Array,
+        required: true
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model("user", userSchema);

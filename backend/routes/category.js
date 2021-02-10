0ae1982600eb3eb736befb018a8e41587541833c
TOLGA <tolgaand@yandex.com>
2021-02-10 17:15:07 +0300

    Change file path CommandCategory to Category

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const Category = require('../models/CommandCategory')
+const Category = require('../models/Category')
+
+router.get('/', async(req, res, next) => {
+    try {
+        return res.json(await Category.find())
+    } catch (error) {
+        return res.json(error.message)
+    }
+})
 
 router.post('/', async(req, res, next) => {
     const { category } = req.body

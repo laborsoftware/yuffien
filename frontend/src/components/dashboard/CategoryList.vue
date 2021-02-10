a9c7d54893ccea9ba87ba41c405973bd71cb258c
TOLGA <tolgaand@yandex.com>
2021-02-10 17:35:45 +0300

    Change 'Home' name to 'CategoryList' and list categories

---
+++ frontend/src/components/dashboard/CategoryList.vue
@@ -0,0 +1,55 @@
+<script>
+import { mapGetters, mapActions, mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters('event', ['guildLogs']),
+    ...mapState('event', ['categories'])
+  },
+  methods: {
+    ...mapActions('event', ['category'])
+  },
+  created() {
+    this.category()
+  }
+}
+</script>
+
+<template lang="pug">
+  #category-list
+    a-row(:gutter="[50,50]")
+      a-col.information-header(:span="24")
+        
+      a-col(:span="24")
+        h1 Sunucumda neler oluyor?
+        a-timeline(pending="Sunucu dinleniyor" :reverse="true")
+          a-timeline-item(v-for = "log in guildLogs" :key = "log.id") {{ log }}
+
+
+      a-col(:span="24")
+        h1 Kategoriler
+      a-col#log(:span="8" v-for = "category in categories" :key="category._id")
+        a-card(:title="`${category.name}`" description = 'hello world' :bordered="false")
+            //- a-affix
+            //-     a-card(title="Sunucumda neler oluyor?"  :bordered="false")
+            //-         ul(v-for = "log in guildLogs")
+            //-             li {{ log }}
+                
+</template>
+
+<style lang="scss" scoped>
+#category-list {
+  h1 {
+    color: #fff;
+    margin-bottom: 0px;
+  }
+  .ant-timeline {
+    color: #fff;
+    padding: 10px;
+  }
+}
+#log {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
